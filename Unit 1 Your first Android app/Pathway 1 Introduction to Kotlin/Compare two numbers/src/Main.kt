916e9099090203164d4674bca2d027cc08505ea3
vuavisao <vuavisao18062005@gmail.com>
2025-09-14 11:53:37 +0700

    Finishing problem: Compare two numbers in Practice Problems: Kotlin Basics in Pathway 1 in Unit 1

---
+++ Unit 1 Your first Android app/Pathway 1 Introduction to Kotlin/Compare two numbers/src/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println(trackScreenTimeVersusYesterday(300, 250))
+    println(trackScreenTimeVersusYesterday(300, 300))
+    println(trackScreenTimeVersusYesterday(200, 220))
+}
+
+fun trackScreenTimeVersusYesterday(timeSpentToday: Int, timeSpentYesterday: Int): Boolean {
+    return (timeSpentToday > timeSpentYesterday)
+}
