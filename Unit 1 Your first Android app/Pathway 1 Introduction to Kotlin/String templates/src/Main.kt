6f62600d22e043b74d2a172cbb9b228390920c5f
vuavisao <vuavisao18062005@gmail.com>
2025-09-14 11:10:22 +0700

    Finishing problem: String templates in Practice Problems: Kotlin Basics in Pathway 1 in Unit 1

---
+++ Unit 1 Your first Android app/Pathway 1 Introduction to Kotlin/String templates/src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val discountPercentage = 20
+    val item = "Google Chromecast"
+    val offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
+
+    println(offer)
+}
