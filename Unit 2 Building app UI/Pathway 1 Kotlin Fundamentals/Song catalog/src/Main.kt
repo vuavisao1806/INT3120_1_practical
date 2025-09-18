a3c8eb50bb5c73519e03248168544bf250766c7e
vuavisao <vuavisao18062005@gmail.com>
2025-09-18 16:38:52 +0700

    Finishing problem: Song catalog in Practice: Kotlin Fundamentals in Pathway 1 in Unit 2

---
+++ Unit 2 Building app UI/Pathway 1 Kotlin Fundamentals/Song catalog/src/Main.kt
@@ -0,0 +1,24 @@
+class Song(
+    val title: String,
+    val artist: String,
+    val yearPublished: Int,
+    val playCount: Int
+) {
+    var isPopular: Boolean = (playCount >= 1000)
+        private set
+
+
+    fun printSongDescription() {
+        println("$title, performed by $artist, was released in $yearPublished.")
+    }
+}
+
+
+fun main() {
+    val songByMtp = Song("Hay trao cho anh", "Son Tung MTP", 2019, 300_000_000)
+    songByMtp.printSongDescription()
+    println("Is ${songByMtp.title} popular? " + songByMtp.isPopular)
+    val songByUnknown = Song("Unknown", "Who", 2025, 100)
+    songByUnknown.printSongDescription()
+    println("Is ${songByUnknown.title} popular? " + songByUnknown.isPopular)
+}
