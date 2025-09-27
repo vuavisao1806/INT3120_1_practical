bf54c2c6af463eac5f4849a0b70faba05d82ec2f
vuavisao <vuavisao18062005@gmail.com>
2025-09-27 12:23:42 +0700

    Finishing Task 7 in Practice: Classes and Collections in Pathway 1 in Unit 3

---
+++ Unit3_Pathway1_Practice_Classes_and_Collections/src/Task7/Main.kt
@@ -0,0 +1,36 @@
+package Task7
+
+enum class Daypart {
+    MORNING, AFTERNOON, EVENING
+}
+
+data class Event(
+    val title: String,
+    val description: String? = null,
+    val daypart: Daypart,
+    val durationInMinutes: Int
+)
+
+val Event.durationOfEvent: String
+    get() = if (durationInMinutes < 60) "short" else "long"
+
+fun main() {
+    val event1 = Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0)
+    val event2 = Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15)
+    val event3 = Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 30)
+    val event4 = Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60)
+    val event5 = Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10)
+    val event6 = Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
+    val events = mutableListOf(
+        event1, event2, event3, event4, event5, event6
+    )
+
+    val durationOfEvent = if (events[0].durationInMinutes < 60) {
+        "short"
+    } else {
+        "long"
+    }
+    println("Duration of first event of the day: $durationOfEvent")
+
+    println("Duration of first event of the day: ${events[0].durationOfEvent}")
+}
