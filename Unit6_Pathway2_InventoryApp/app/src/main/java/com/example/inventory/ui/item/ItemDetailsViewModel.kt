0f66bdfaffea9992df48bc15aead9382f5602fd0
vuavisao <vuavisao18062005@gmail.com>
2025-10-15 19:19:13 +0700

    Finishing adding persist data with Room for an Inventory app in Pathway 2 in Unit 6

---
+++ Unit6_Pathway2_InventoryApp/app/src/main/java/com/example/inventory/ui/item/ItemDetailsViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.inventory.ui.item
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+
+/**
+ * ViewModel to retrieve, update and delete an item from the [ItemsRepository]'s data source.
+ */
+class ItemDetailsViewModel(
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    private val itemId: Int = checkNotNull(savedStateHandle[ItemDetailsDestination.itemIdArg])
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
+/**
+ * UI state for ItemDetailsScreen
+ */
+data class ItemDetailsUiState(
+    val outOfStock: Boolean = true,
+    val itemDetails: ItemDetails = ItemDetails()
+)
---
+++ Unit6_Pathway2_InventoryApp/app/src/main/java/com/example/inventory/ui/item/ItemEditViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.inventory.ui.item
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+
+/**
+ * ViewModel to retrieve and update an item from the [ItemsRepository]'s data source.
+ */
+class ItemEditViewModel(
+    savedStateHandle: SavedStateHandle,
+) : ViewModel() {
+
+    /**
+     * Holds current item ui state
+     */
+    var itemUiState by mutableStateOf(ItemUiState())
+        private set
+
+    private val itemId: Int = checkNotNull(savedStateHandle[ItemEditDestination.itemIdArg])
+
+    private fun validateInput(uiState: ItemDetails = itemUiState.itemDetails): Boolean {
+        return with(uiState) {
+            name.isNotBlank() && price.isNotBlank() && quantity.isNotBlank()
+        }
+    }
+}
